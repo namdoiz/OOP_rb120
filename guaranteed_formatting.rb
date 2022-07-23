2efb5b2b7b1b1ee44d806dc5b3e57fd987accab2
namdoiz <nnamdi.on7@gmail.com>
2022-07-23 03:17:51 -0500

    Using the following code, add the appropriate accessor methods so that @name is capitalized upon assignment

---
+++ guaranteed_formatting.rb
@@ -0,0 +1,15 @@
+# Using the following code, add the appropriate accessor methods so that @name is capitalized upon assignment
+
+class Person
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name.capitalize
+  end
+end
+
+person1 = Person.new
+person1.name = 'eLiZaBeTh'
+puts person1.name
